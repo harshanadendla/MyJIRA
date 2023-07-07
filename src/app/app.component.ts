815959e20ada276d057307896c256c838db15976
Harsha Nadendla <chinnunadendla@gmail.com>
2023-07-07 00:03:42 -0500

    name change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'harshi-harsha';
+  title = 'MyJIRA';
   date = new Date();
   name = 'HarshaHarshi';
 }
